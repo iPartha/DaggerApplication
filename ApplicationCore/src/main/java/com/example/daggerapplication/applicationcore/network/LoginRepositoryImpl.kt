08bc420f6dee394877773387decbd7ccd033422c
Parthasarathi Kathiresan <parthasarathi.kathiresan@hidglobal.com>
2021-05-19 18:47:34 +0530

    Gradle changes

--- ApplicationCore/src/main/java/com/example/daggerapplication/applicationcore/network/LoginRepositoryImpl.kt
+++ ApplicationCore/src/main/java/com/example/daggerapplication/applicationcore/network/LoginRepositoryImpl.kt
@@ -21,7 +21,6 @@ class LoginRepositoryImpl @Inject constructor (
         // If user credentials will be cached in local storage, it is recommended it be encrypted
         // @see https://developer.android.com/training/articles/keystore
         println("Partha")
-        println("Partha sarathi")
         user = null
     }
 
