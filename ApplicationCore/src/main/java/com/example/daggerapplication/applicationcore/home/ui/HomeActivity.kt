68f02b301290af9a454ada659a89c15f6285689e
Parthasarathi Kathiresan <parthasarathi.kathiresan@hidglobal.com>
2021-05-28 17:19:09 +0530

    Initial commit

--- ApplicationCore/src/main/java/com/example/daggerapplication/applicationcore/home/ui/HomeActivity.kt
+++ ApplicationCore/src/main/java/com/example/daggerapplication/applicationcore/home/ui/HomeActivity.kt
@@ -14,6 +14,7 @@ class HomeActivity : DaggerFragmentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        println("Partha Test")
         setContentView(R.layout.activity_home)
         homeViewModel.getHomeMenuData()
     }
